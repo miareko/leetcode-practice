aa49df0f03eed299d890489a91b7da4081c12870
lu.fan <lu.fan@supwisdom.com>
2016-08-11 00:35:09 +0800

    p2:Longest Substring Without Repeating Characters

---
+++ src/me/dddew/leetcode/p003/Solution.java
@@ -0,0 +1,67 @@
+package me.dddew.leetcode.p003;
+
+
+/**
+ * 3. Longest Substring Without Repeating Characters
+ * @see <a href="https://leetcode.com/problems/longest-substring-without-repeating-characters/">https://leetcode.com/problems/longest-substring-without-repeating-characters/</a>
+ * 
+ * Given a string, find the length of the longest substring without repeating characters.
+ * 
+ * Examples:
+ * 
+ * Given "abcabcbb", the answer is "abc", which the length is 3.
+ * Given "bbbbb", the answer is "b", with the length of 1.
+ * Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ *
+ */
+public class Solution {
+	
+	/**
+	 * 从左往右扫描，假设当前字符是最长不重复子串的最后一个字符，取最大值。
+	 */
+	public int lengthOfLongestSubstring(String s) {
+		
+		if(s == null) {
+			return 0;
+		}
+		
+		int[] lastIndex = new int[256]; // 字符最后一次出现的位置
+		int ans = 0;
+		for(int i = 0, lastPos = 0; i < s.length(); i++) {
+			lastPos = Math.max(lastIndex[s.charAt(i)], lastPos);
+			ans = Math.max(ans, i - lastPos + 1);
+			lastIndex[s.charAt(i)] = i+1;
+		}
+		
+		return ans;
+	}
+
+	/**
+	 * 从左往右扫描，当遇到重复字母时，以上一个重复字母的index+1，作为新的搜索起始位置
+	 */
+	public int lengthOfLongestSubstring2(String s) {
+        
+		int length = 0;
+		
+		if(s != null) {
+			int from = 0;
+			for(int i = 0; i < s.length(); i++) {
+				
+				for(int j = from; j <= i - 1; j++) {
+					
+					if(s.charAt(i) == s.charAt(j)) {
+						length = Math.max(i - from, length);
+						from = j + 1;
+					}
+				}
+				
+				if(i == s.length() - 1) {
+					length = Math.max(i + 1 - from, length);
+				}
+			}
+		}
+		
+		return length;
+    }
+	
+}
