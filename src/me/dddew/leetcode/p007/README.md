3d38170156a0c901b0f8db58e738892efbb5b917
lu.fan <lu.fan@supwisdom.com>
2016-08-29 15:10:36 +0800

    p7:Reverse Integer

---
+++ src/me/dddew/leetcode/p007/README.md
@@ -0,0 +1,15 @@
+## Reverse Integer
+
+[https://leetcode.com/problems/reverse-integer/](https://leetcode.com/problems/reverse-integer/)
+
+Reverse digits of an integer.
+
+**Example1**: x = 123, return 321
+
+**Example2**: x = -123, return -321
+
+<br>
+
+### 解题思路
+
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;主要是注意反序之后的溢出问题
---
+++ src/me/dddew/leetcode/p007/Solution.java
@@ -0,0 +1,32 @@
+package me.dddew.leetcode.p007;
+
+/**
+ * created by fanlu on 08/12/2016
+ */
+public class Solution {
+
+    public int reverse(int x) {
+    	
+    	if(x == 0) {
+    		return 0;
+    	}
+    	
+    	int abs = 1;
+    	if(x < 0) {
+    		abs = -1;
+    	}
+    	x = Math.abs(x);
+    	
+    	int y = 0;
+    	while(x > 0) {
+    		y = y*10 + x%10;
+    		x /= 10;
+    		
+    		if(x < 10 && x > 0 && y > 0 && (Integer.MAX_VALUE - x)/y < 10) {
+    			return 0;
+    		}
+    	}
+    	return abs * y;
+    }
+    
+}
